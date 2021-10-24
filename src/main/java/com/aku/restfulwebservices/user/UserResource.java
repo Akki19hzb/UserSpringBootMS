82f78d8650aceae6b0e7d16f4c8c53799f44133f
Akki19hzb <getakash.verma19@gmail.com>
2021-10-25 00:48:07 +0530

    Generic Exception handler added

--- src/main/java/com/aku/restfulwebservices/user/UserResource.java
+++ src/main/java/com/aku/restfulwebservices/user/UserResource.java
@@ -1,14 +1,17 @@
 package com.aku.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api")
@@ -22,13 +25,24 @@ public class UserResource {
 		return service.findAll();
 	}
 	
-	@GetMapping("/user/{id}")
+	@GetMapping("/users/{id}")
 	public User retriveAllUsers(@PathVariable int id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user==null) {
+			throw new UserNotFoundException("id- "+id);
+		}
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public User retriveAllUsers(@RequestBody User user){
-		return service.save(user);
+	public ResponseEntity<Object> retriveAllUsers(@RequestBody User user){
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		 
+		return ResponseEntity.created(location).build();
 	}
 }
