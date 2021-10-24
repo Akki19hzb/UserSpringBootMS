ef15df3faa1a143d82909ef673b3d39f8358db01
Akki19hzb <getakash.verma19@gmail.com>
2021-10-25 00:57:25 +0530

    delete user controller added

--- src/main/java/com/aku/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/aku/restfulwebservices/user/UserDaoService.java
@@ -2,6 +2,7 @@ package com.aku.restfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -42,5 +43,15 @@ public class UserDaoService {
 		return null;
 	}
 	
-
+	public User deleteById(int id) {
+		Iterator<User> itr = users.iterator();
+		while (itr.hasNext()) {
+			User user = itr.next();
+			if (user.getId() == id) {
+				itr.remove();
+				return user;
+			}
+		}
+		return null;
+	}
 }
